712505a765950ed5ddb5c2859cf79ca9d791bd25
Bob Bonifield <bobbonifield@gmail.com>
2012-02-17 22:54:23 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IosPlague::Application.config.secret_token = 'a0f2d0a22f940438bf366e42683f483868a2fdd311955b1c982bd7d495ef3f20971ef9d1453438abc2b56ac9070abdb09649a8099d9b4e8142862fe0b2cca05e'
