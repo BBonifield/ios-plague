0b2e0d50e89844dda4a62145509eba37bc18227f
Bob Bonifield <bobbonifield@gmail.com>
2012-02-18 13:02:52 -0700

    a

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,3 @@ IosPlague::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 end
-
-require 'pusher'
-
-Pusher.app_id = '15658'
-Pusher.key = 'ce0f7e9d17c0e1cf9eef'
-Pusher.secret = '976e225ab854e25087b0'
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,5 @@
+require 'pusher'
+
+Pusher.app_id = '15658'
+Pusher.key = 'ce0f7e9d17c0e1cf9eef'
+Pusher.secret = '976e225ab854e25087b0'
